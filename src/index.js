140b4d5e9af85f5656e590360be0e4f2332babcf
Eyal Eizenberg <eyal.eizenberg+github@samanage.com>
2016-06-26 12:02:57 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const comparePropsAndState = (component, prevProps, prevState) => {
+  const propChanges = [];
+  const stateChanges = [];
+
+  for (let key in prevProps) {
+    if (component.props[key] !== prevProps[key]) propChanges.push(key)
+  }
+
+  for (let key in prevState) {
+    if (component.state[key] !== prevState[key]) prevState.push(key)
+  }
+
+  return { propChanges, stateChanges };
+}
+
+export default function logAllUpdates() {
+  return function wrap(ReactClass) {
+    const originalComponentDidMount = ReactClass.prototype.componentDidMount;
+    const originalComponentDidUpdate = ReactClass.prototype.componentDidUpdate;
+
+    ReactClass.prototype.componentDidMount = function componentDidMount() {
+      this.previousRenderTimeStamp = new Date();
+      if (originalComponentDidMount) {
+        return originalComponentDidMount.apply(this, arguments);
+      }
+    }
+
+    ReactClass.prototype.componentDidUpdate = function componentDidUpdate(prevProps, prevState) {
+      const newRenderTimeStamp = new Date();
+      const timeRenderDiff = newRenderTimeStamp - this.previousRenderTimeStamp;
+      const threshold = this.warningThreshold || 200
+      if (timeRenderDiff < threshold) {
+        const diff = comparePropsAndState(this, prevProps, prevState);
+        console.groupCollapsed(`The component ${this.constructor.name} rendered twice in less than ${threshold}ms!`);
+        console.info('Time between renders:', timeRenderDiff);
+        console.info('Props key changes:', diff.propChanges);
+        console.info('State key changes:', diff.stateChanges);
+        console.groupEnd();
+      }
+
+      this.previousRenderTimeStamp = newRenderTimeStamp;
+
+      if (originalComponentDidUpdate) {
+        return originalComponentDidUpdate.apply(this, arguments);
+      }
+    }
+
+    return ReactClass;
+  }
+}
